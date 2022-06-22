5a60f888924986b64dba9ebab9af6411a222dd3c
Himanshu <himanshugoswami.12feb@gmail.com>
2022-06-23 00:26:24 +0530

    Font Size changed

--- config.def.h
+++ config.def.h
@@ -5,10 +5,10 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "RecMonoCasual Nerd Font:pixelsize=18:antialias=true:autohint=true";
+static char *font = "RecMonoCasual Nerd Font:pixelsize=16:antialias=true:autohint=true";
 /* Spare fonts */
 static char *font2[] = {
-	"JoyPixels:pixelsize=18:antialias=true:autohint=true", 
+	"JoyPixels:pixelsize=16:antialias=true:autohint=true", 
 /*	"Hack Nerd Font Mono:pixelsize=11:antialias=true:autohint=true", */
 };
 
